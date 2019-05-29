fef2cc44d616d574e2c0ab7d4238cd453c4ebac6
Vlad <rodikovvlad@gmail.com>
2019-05-29 22:23:08 +0700

    Add files

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from random import choice
 from colorama import init
-from colorama import Fore, Back
+from colorama import Fore
 import requests, bs4
-import re
+from time import sleep
+
 
 init(autoreset=True)
 
@@ -34,15 +35,18 @@ def set_words():
 def start(word, mass_symb):
     mistakes = 0
     count = 0
+    inp_letters = set([])
     for i in range(20):
         if count == len(word):
-            print(Fore.GREEN+"!!!!!!--WIN--!!!!!!")
+            print(Fore.GREEN+"!!!!!!----------ПОБЕДА---------!!!!!!")
             break
         print()
-        letter = input(Fore.BLACK+Back.GREEN+"Введите букву: ")
+        letter = input("Введите букву: ").lower()
+        inp_letters.add(letter)
         print()
         if letter in mass_symb:
-            print(Fore.RED+"Введите другую букву!")
+            print(Fore.RED+"ЭТА БУКВА УЖЕ ЕСТЬ В СЛОВЕ!")
+            print(Fore.RED+"ВВЕДИТЕ ДРУГУЮ БУКВУ!")
         else:
             for k in range(len(word)):
                 if letter == word[k]:
@@ -50,27 +54,47 @@ def start(word, mass_symb):
                     count += 1
             for l in mass_symb:
                 print(l + " ", end="")
+            print(Fore.CYAN+"  <------ СЛОВО, КОТОРОЕ НАДО УГАДАТЬ")
             if letter not in mass_symb:
                 mistakes += 1
             print()
-            print(Fore.YELLOW+f"Кол-во неверных попыток: {mistakes}")
+            print(Fore.YELLOW+"КОЛ-ВО НЕВЕРНЫХ ПОПЫТОК: "+Fore.GREEN+str(mistakes), end="")
+            print(" | "+ Fore.YELLOW+f"ОСТАЛОСЬ ПОПЫТОК: {20-i}")
+            print(Fore.YELLOW+"ВВЕДЕННЫЕ БУКВЫ: ", end="")
+            print(Fore.GREEN,end="")
+            print(inp_letters)
             print()
-
 
 # Создание подчеркиваний
 def set_symbols(word):
     mass_symb = []
     for let in word:
         mass_symb.append("_")
+    print()
     for i in mass_symb:
         print(i + " ", end="")
+    print(Fore.CYAN + "  <------ СЛОВО, КОТОРОЕ НАДО УГАДАТЬ")
     print()
     return mass_symb
 
 
 # Главная функция
 def main():
-    print(Fore.YELLOW + "------------------------ПОДОЖДИТЕ НЕМНОГО------------------------")
+    print(Fore.GREEN + "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\------ ВИСЕЛИЦА ------////////////////////////")
+    sleep(1)
+    print()
+    print(Fore.BLUE + "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\------ БЕТА ВЕРСИЯ ------/////////////////////")
+    sleep(1)
+    print()
+    print(Fore.YELLOW + "------------------------------------ ПРАВИЛА ---------------------------------")
+    print(Fore.YELLOW+"|                                                                            |")
+    print(Fore.YELLOW+"|"+Fore.GREEN + "   1 - ВАМ НАДО УГАДАТЬ СЛОВО, БУКВЫ КОТОРОГО СКРЫТЫ СИМВОЛОМ ПОДЧЕРКИВАНИЯ "
+          +Fore.YELLOW+"|")
+    print(Fore.YELLOW + "|" + Fore.GREEN + "   2 - У вас есть 20 попыток                                              "
+                                           "  " + Fore.YELLOW + "|")
+    print(Fore.YELLOW + "|" + Fore.GREEN + "   3 - Вводите по одной букве                                            "
+                                           "   " + Fore.YELLOW + "|")
+    print(Fore.YELLOW + "------------------------------------------------------------------------------")
     words = set_words()
     word = choice(words)
     mass_symb = set_symbols(word)
